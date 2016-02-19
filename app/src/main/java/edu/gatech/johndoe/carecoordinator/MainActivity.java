c87bfd3172d364d36ea7d5f9774bd02f3d3d3279
Hyun Seo Chung <hyunseo0404@gmail.com>
2016-02-18 19:37:37 -0500

    Updated placeholder values, added an example label with the current tab name, added paddings to fragments, and combined fragment interaction listeners to a separate OnFragmentInteractionListener interface

--- app/src/main/java/edu/gatech/johndoe/carecoordinator/MainActivity.java
+++ app/src/main/java/edu/gatech/johndoe/carecoordinator/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.gatech.johndoe.carecoordinator;
 
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,15 +12,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -89,41 +86,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -135,28 +97,57 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return SummaryFragment.newInstance("Summary Fragment");
+                case 1:
+                    return ReferralFragment.newInstance("Referral Fragment");
+                case 2:
+                    return PatientFragment.newInstance("Patient Fragment");
+                case 3:
+                    return CommunityFragment.newInstance("Community Fragment");
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 4;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Summary";
                 case 1:
-                    return "SECTION 2";
+                    return "Referrals";
                 case 2:
-                    return "SECTION 3";
+                    return "Patients";
+                case 3:
+                    return "Community";
             }
             return null;
         }
+    }
+
+    @Override
+    public void onSummaryFragmentInteraction(Uri uri) {
+        // TODO
+    }
+
+    @Override
+    public void onReferralFragmentInteraction(Uri uri) {
+        // TODO
+    }
+
+    @Override
+    public void onPatientFragmentInteraction(Uri uri) {
+        // TODO
+    }
+
+    @Override
+    public void onCommunityFragmentInteraction(Uri uri) {
+        // TODO
     }
 }
---
+++ app/src/main/java/edu/gatech/johndoe/carecoordinator/OnFragmentInteractionListener.java
@@ -0,0 +1,10 @@
+package edu.gatech.johndoe.carecoordinator;
+
+import android.net.Uri;
+
+public interface OnFragmentInteractionListener {
+    void onSummaryFragmentInteraction(Uri uri);
+    void onReferralFragmentInteraction(Uri uri);
+    void onPatientFragmentInteraction(Uri uri);
+    void onCommunityFragmentInteraction(Uri uri);
+}
