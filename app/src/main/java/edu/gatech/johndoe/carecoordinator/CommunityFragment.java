5c8abaf5cdebd9e32e4f6cb49ff8e28df6a3f24c
rakyu012 <bpark31@gatech.edu>
2016-02-23 16:34:25 -0500

    update

--- app/src/main/java/edu/gatech/johndoe/carecoordinator/CommunityFragment.java
+++ app/src/main/java/edu/gatech/johndoe/carecoordinator/CommunityFragment.java
@@ -55,9 +55,9 @@ public class CommunityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_referral, container, false);
+        View view = inflater.inflate(R.layout.fragment_community, container, false);
         TextView exampleLabel = (TextView) view.findViewById(R.id.exampleLabel);
-        exampleLabel.setText(mExampleParam);
+//        exampleLabel.setText(mExampleParam);
         return view;
     }
 
